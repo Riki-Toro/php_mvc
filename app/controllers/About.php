f12bb8dd89c06ed3234be4874401125bb9d353fc
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-24 11:48:14 +0700

    Create About.php

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About {
+
+    public function index($nama = 'Riki Widiantoro', $pekerjaan = 'Web Programmer') {
+        echo "Hallo, Nama saya $nama, saya seorang $pekerjaan";
+    }
+    public function page() {
+        echo "About/Page";
+    }
+}
+
+
+?>
