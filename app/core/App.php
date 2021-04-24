e12606dceec66fd900398c5646613bc714a19baa
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-24 10:33:27 +0700

    Update App.php

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,22 @@
 
 class App {
     public function __construct() {
-        echo "ok!";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL() {
+        if( isset($_GET['url']) ) {
+            // membersihkan atau menghilangkan tanda '/' diakhir url dengan fungsi rtrim()
+            $url = rtrim($_GET['url'], '/');
+
+            // membersihkan url dari karakter yang ngaco/dihack
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            // pecah url berdasarkan tanda '/' dengan fungsi explode()
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
 
