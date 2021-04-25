de8af1cf9988a88d20d3fea2a08528dbc0f8f0ad
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-25 14:34:09 +0700

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,7 +1,10 @@
 <?php
 
 class Controller {
-    
+    public function view($view, $data = []) {
+        // panggil folder view
+        require_once '../app/views/' . $view . '.php';
+    }
 }
 
 ?>
