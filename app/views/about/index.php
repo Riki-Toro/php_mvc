1e2c288a06b8afb629b23f1895d9f1cf3b8171e3
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-26 15:14:53 +0700

    Update index.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
 
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-3">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Riki Widiantoro" width="100" class="rounded-circle shadow-lg rounded">
     <p>Halo nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'];?> dan saya berumur <?= $data['umur']; ?> tahun.</p>
+</div>
