936e39eb3244a8633cc053a2ed18fa3ffa96b5a2
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-27 11:38:41 +0700

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -18,7 +18,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="<?= BASEURL; ?>/homes">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link active" href="<?= BASEURL; ?>/home">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
             <!-- <a class="nav-item nav-link" href="#">Pricing</a>
             <a class="nav-item nav-link disabled" href="#">Disabled</a> -->
