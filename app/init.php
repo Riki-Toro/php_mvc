e6c93014f5aea505652c99ee968b27ced6438536
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-28 15:18:27 +0700

    Update init.php

--- app/init.php
+++ app/init.php
@@ -2,6 +2,7 @@
 // 2 class pembentuk utama
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+require_once 'core/Database.php';
 
-require_once 'core/Constants.php';
+require_once 'config/config.php';
 ?>
