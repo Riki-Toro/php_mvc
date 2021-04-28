38223e63b51610b7e42146b0e4857ffff094c446
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-28 15:19:25 +0700

    Update Mahasiswa_model.php

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,44 +1,21 @@
 <?php
 class Mahasiswa_model {
-    // private $mhs = [
-    //     [
-    //         "nama" => "Riki Widiantoro",
-    //         "nim" => "111111",
-    //         "jurusan" => "Teknik Informatika"
-    //     ],
-    //     [
-    //         "nama" => "Eren Yeager",
-    //         "nim" => "222222",
-    //         "jurusan" => "Teknik Industri"
-    //     ],
-    //     [
-    //         "nama" => "Uchiha Sasuke",
-    //         "nim" => "333333",
-    //         "jurusan" => "Sistem Informasi"
-    //     ]
-    // ];
+    private $tabel = 'mahasiswa';
+    private $db;
 
-    // koneksi ke database menggunakan PDO (php data object) lebih simpel kedepannya >> dengan pdo akan lebih mudah dibandingkan driver nya mysqli
-    private $dbh; // database handler
-    private $stmt; // untuk menyimpan query
-
-    // koneksi ke database dengan method construct
     public function __construct() {
-        // data source name = koneksi ke database
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', ''); // jika berhasil masuk database
-        } catch( PDOException $e ) {
-            die($e->getMessage()); // jika error hentikan program lalu kasih pesan error
-        }
+        $this->db = new Database;
     }
 
     public function getAllMahasiswa() {
-        // untuk mendapatkan nya kita butuh query
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa'); // jalanin query
-        $this->stmt->execute(); // eksekusi query
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->tabel);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id) {
+        $this->db->query('SELECT * FROM ' . $this->tabel . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
 
