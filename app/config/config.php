29091dbb1a531230a1e896905a197d3e5e9040fa
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-28 15:18:02 +0700

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<!-- untuk menyimpan constanta-constanta yang dibutuhkan -->
+<?php
+// simpan url absolute
+define('BASEURL', 'http://localhost/11.phpmvc/public');
+
+// database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+?>
