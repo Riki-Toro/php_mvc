8028037d49b22b3fa4663b97a468978123a753be
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-29 13:54:05 +0700

    Update detail.php

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -4,7 +4,7 @@
     <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title"><?= $data['mhs']['nama']; ?></h5>
-            <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nim']; ?></h6>
+            <p class="card-text"><?= $data['mhs']['nim']; ?></p>
             <p class="card-text"><?= $data['mhs']['jurusan']; ?></p>
             <a href="<?= BASEURL; ?>/mahasiswa" class="card-link">Kembali</a>
         </div>
