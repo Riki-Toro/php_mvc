8089776049377db062d30e0870ab0ff62dc955f5
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-30 14:35:46 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+// jalankan session
+if( !session_id() ) {
+    session_start();
+}
+
 require_once '../app/init.php'; //teknik bootstraping cuma memanggil file init dan init akan memanggil seluruh file app
 
 $app = new App;
